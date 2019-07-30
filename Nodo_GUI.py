8378b1668fa89b1b2bee8b4b032da4af847de1aa
Rodrigo Garcia Lopez <44546412+Rodrigo-Garci@users.noreply.github.com>
2019-07-30 14:28:31 -0500

    Update Nodo_GUI.py
    Grid view is not compatible with pack()

--- Nodo_GUI.py
+++ Nodo_GUI.py
@@ -1,26 +1,45 @@
 #!/usr/bin/env python
 
 import Tkinter as tk
+from Tkinter import *
 import rospy
 from std_msgs.msg import Int32
 
 class App():
     def __init__(self):
         self.root = tk.Tk()
-        self.root.geometry("250x250") #change the size of the window
+        self.root.geometry("700x394")
+        self.root.wm_title("Serial Communication") #Makes the title that will appear in the top left
+        self.root.config(background = "#FFFFFF")
         rospy.init_node('listener')
         self.sub = rospy.Subscriber("chatter", Int32, self.NowLoading)
         self.label = tk.Label(text="")
-        self.label.pack()
+
+        #Left Frame and its contents
+        self.leftFrame = Frame(self.root, width=300, height = 600, bg="white")
+        self.leftFrame.grid(row=0, column=0, padx=10, pady=2)
+
+        Label(self.leftFrame, text="Instructions",fg="black",bg="white",font=("Arial")).grid(row=0, column=0, padx=10, pady=2)
+
+        Instruct = Label(self.leftFrame, text="1. Run the Interface\n2. Run the Node that increase the value\n")
+        Instruct.grid(row=1, column=0, padx=10, pady=2)
+
+        #Right Frame and its contents
+        self.rightFrame = Frame(self.root, width=300, height = 600, bg="white")
+        self.rightFrame.grid(row=0, column=1, padx=10, pady=2)
+
+        Label(self.rightFrame, text="Datos:",fg="black",bg="white",font=("Arial")).grid(row=0, column=0, padx=10, pady=2)
+
+
         self.root.mainloop()
 
 
     def NowLoading(self, data):
-	   #rospy.loginfo("The value is: %i", Garcia.data)
-       #now = str(se
+
        now = data.data
-       self.label.configure(text=now)
-       self.label.place(x=20, y=30) #to move the label
+       Increasing = Label(self.rightFrame, text=now, bg="white", fg="black")
+       Increasing.grid(row=1, column=0,padx=10,pady=2)
+
        self.root.after(1000, self, self.NowLoading)
 
 
