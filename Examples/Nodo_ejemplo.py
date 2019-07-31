1de972d622e58475dba31ab3a5a2df50aa4a4dc2
Edwin Miguel Hoil Majay <53419453+edwin221b@users.noreply.github.com>
2019-07-31 00:46:40 -0500

    Add files via upload

--- Examples/Nodo_ejemplo.py
+++ Examples/Nodo_ejemplo.py
@@ -1,60 +1,39 @@
 #!/usr/bin/env python
-from Tkinter import *
+from tkinter import *
 
-root = Tk() #Makes the window
-root.wm_title("Window Title") #Makes the title that will appear in the top left
-root.config(background = "#FFFFFF")
+from PIL import Image, ImageTk
 
+master = Tk()
+master.geometry("1280x720")
 
-def redCircle():
-    circleCanvas.create_oval(20, 20, 80, 80, width=0, fill='red')
-    colorLog.insert(0.0, "Red\n")
+def callback():
+    #print "click!"
+    load = Image.open("hola.jpg")
+    render = ImageTk.PhotoImage(load)
+    img = Label( image=render)
+    img.image = render
+    img.place(x=0, y=0)
 
-def yelCircle():
-    circleCanvas.create_oval(20, 20, 80, 80, width=0, fill='yellow')
-    colorLog.insert(0.0, "Yellow\n")
-
-def grnCircle():
-    circleCanvas.create_oval(20, 20, 80, 80, width=0, fill='green')
-    colorLog.insert(0.0, "Green\n")
-
-
-#Left Frame and its contents
-leftFrame = Frame(root, width=200, height = 600)
-leftFrame.grid(row=0, column=0, padx=10, pady=2)
-
-Label(leftFrame, text="Instructions:").grid(row=0, column=0, padx=10, pady=2)
-
-Instruct = Label(leftFrame, text="1\n2\n2\n3\n4\n5\n6\n7\n8\n9\n")
-Instruct.grid(row=1, column=0, padx=10, pady=2)
-
-try:
-    imageEx = PhotoImage(file = 'image.gif')
-    Label(leftFrame, image=imageEx).grid(row=2, column=0, padx=10, pady=2)
-except:
-    print("Image not found")
-
-#Right Frame and its contents
-rightFrame = Frame(root, width=200, height = 600)
-rightFrame.grid(row=0, column=1, padx=10, pady=2)
-
-circleCanvas = Canvas(rightFrame, width=100, height=100, bg='white')
-circleCanvas.grid(row=0, column=0, padx=10, pady=2)
-
-btnFrame = Frame(rightFrame, width=200, height = 200)
-btnFrame.grid(row=1, column=0, padx=10, pady=2)
-
-colorLog = Text(rightFrame, width = 30, height = 10, takefocus=0)
-colorLog.grid(row=2, column=0, padx=10, pady=2)
-
-redBtn = Button(btnFrame, text="Red", command=redCircle)
-redBtn.grid(row=0, column=0, padx=10, pady=2)
-
-yellowBtn = Button(btnFrame, text="Yellow", command=yelCircle)
-yellowBtn.grid(row=0, column=1, padx=10, pady=2)
-
-greenBtn = Button(btnFrame, text="Green", command=grnCircle)
-greenBtn.grid(row=0, column=2, padx=10, pady=2)
+def callback2():
+    #print "click!"
+    load = Image.open("hud.png")
+    render = ImageTk.PhotoImage(load)
+    img = Label( image=render)
+    img.image = render
+    img.place(x=0, y=0)
 
 
-root.mainloop() #start monitoring and updating the GUI
+
+
+a = Button(master, text="hola", command=callback2)
+b = Button(master, text="OK", command=callback)
+b.pack()
+a.pack()
+
+mainloop()
+
+root = Tk()
+app = Window(root)
+root.wm_title("Tkinter window")
+root.geometry("1000x1000")
+root.mainloop()
